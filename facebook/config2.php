ea3539bea13c9b7d294098b811c3be5c679ca372
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-18 13:18:01 +0800

    Update config2.php

--- facebook/config2.php
+++ facebook/config2.php
@@ -10,8 +10,8 @@ if (!session_id())
 // Call Facebook API 
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '568286157120937',
-  'app_secret'     => '1e3a40ac75b79a33fbf48f70731d4616',
+  'app_id'      => '1433293860183714',
+  'app_secret'     => '88885424900cebc1b9c852e544613dc2Reset',
   'default_graph_version'  => 'v2.10'
 ]);
 
