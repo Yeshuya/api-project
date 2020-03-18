4c82d4a3833a1c51be221e8c66c67ab54062ba90
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-18 13:25:52 +0800

    Update config2.php

--- config2.php
+++ config2.php
@@ -1,12 +1,17 @@
-<?php
+<?php 
 
 require_once 'vendor/autoload.php';
 
-// Call Facebook API
+if (!session_id())
+{
+    session_start();
+}
+
+// Call Facebook API 
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '2621185531323878',
-  'app_secret'     => 'e96b253c6d17a45a674710eefe2153f1',
+  'app_id'      => '1433293860183714',
+  'app_secret'     => '88885424900cebc1b9c852e544613dc2Reset',
   'default_graph_version'  => 'v2.10'
 ]);
 
