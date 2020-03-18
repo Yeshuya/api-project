eecf2938077dc93c0bbae1463d4cc948d7e9664a
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-18 12:47:54 +0800

    Add files via upload

---
+++ google/config.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'vendor/autoload.php';
+
+//google ------ credits: john paul maja :D <3 
+$google_client = new Google_Client();
+$google_client->setClientId('862564044784-fbpmelidspimkssnl0etfdcsp76510nc.apps.googleusercontent.com');
+$google_client->setClientSecret('Gos4kW0_ZZAnmUdQMvsTCocA');
+$google_client->setRedirectUri('http://localhost/Projects/Api%20Project/index.php');
+$google_client->addScope('email');
+$google_client->addScope('profile');
+
+
+session_start();
+// credits: john paul majaaaaaa
+?>
